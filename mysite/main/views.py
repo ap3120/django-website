95f4e1a659d3273f04aa5c9215f0186658eb972c
dev <ygbe@hotmail.fr>
2023-03-22 10:38:47 +0000

    restrict page access

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -7,36 +7,36 @@ from .forms import CreateListForm
 
 def index(request, id):
     ls = ToDoList.objects.get(id=id)
-    #if ls in response.user.todolist.all():
-    if request.method == "POST":
-        if request.POST.get("save"):
-            for item in ls.item_set.all():
-                p = request.POST
+    if request.user.is_authenticated and ls in request.user.todolist.all():
+        if request.method == "POST":
+            if request.POST.get("save"):
+                for item in ls.item_set.all():
+                    p = request.POST
 
-                if "clicked" == p.get("c"+str(item.id)):
-                    item.complete = True
+                    if "clicked" == p.get("c"+str(item.id)):
+                        item.complete = True
+                    else:
+                        item.complete = False
+
+                    if "text" + str(item.id) in p:
+                        item.text = p.get("text" + str(item.id))
+
+
+                    item.save()
+
+            elif request.POST.get("add"):
+                newItem = request.POST.get("new")
+                if newItem != "":
+                    ls.item_set.create(text=newItem, complete=False)
                 else:
-                    item.complete = False
-
-                if "text" + str(item.id) in p:
-                    item.text = p.get("text" + str(item.id))
+                    print("invalid")
 
 
-                item.save()
-
-        elif request.POST.get("add"):
-            newItem = request.POST.get("new")
-            if newItem != "":
-                ls.item_set.create(text=newItem, complete=False)
-            else:
-                print("invalid")
-
-
-    return render(request, "main/index.html", {"ls": ls})
-#return render(request, "main/view.html")
+        return render(request, "main/index.html", {"ls": ls})
+    return render(request, "main/view.html")
 
 def create(request):
-    if request.method == "POST":
+    if request.method == "POST" and user.is_authenticated:
         form = CreateListForm(request.POST)
 
         if form.is_valid():
